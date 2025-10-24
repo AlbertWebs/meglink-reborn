4aab59d6d800e9ca80000aa99ab66fe74238b2e4
AlbertWebs <albertmuhatia@gmail.com>
2025-10-24 12:49:24 +0300

    update

--- resources/views/components/full-homepage-banner.blade.php
+++ resources/views/components/full-homepage-banner.blade.php
@@ -47,7 +47,7 @@
             text-align: center;
             max-width: 850px;
             margin: 0 20px;
-            backdrop-filter: blur(8px); /* Enabled for modern browsers */
+            /* backdrop-filter: blur(8px);  */
             box-shadow: 0 15px 30px rgba(0, 0, 0, 0.3);
             border: 1px solid rgba(255, 255, 255, 0.1);
             transform: translateY(20px);
